d034ac07a9ca51ec8debfdcb1bda0c8fbc090da3
Brian Jacobel <bjacobel@gmail.com>
2019-11-11 20:31:50 -0500

    Use HTTP2 and a self-signed cert for lighthouse tests

--- src/commands/lighthouse.ts
+++ src/commands/lighthouse.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { promises } from 'fs';
 import path from 'path';
 import lighthouse from 'lighthouse';
 import * as chromeLauncher from 'chrome-launcher';
@@ -7,6 +7,11 @@ import staticfiles from 'koa-static';
 import compress from 'koa-compress';
 import opn from 'open';
 import { Argv, CommandBuilder } from 'yargs';
+import http2 from 'http2';
+
+import { key, cert, caCrt } from '../utils/ssl';
+
+const fs = promises;
 
 const webpackCompile = async (webpack, config) => {
   const compiler = webpack(config);
@@ -23,54 +28,64 @@ const webpackCompile = async (webpack, config) => {
 
 export const command = 'lighthouse';
 
-export const describe = 'runs the Lighthouse perf tool and opens results in the browser';
+export const describe =
+  'runs the Lighthouse perf tool and opens results in the browser';
 
 export const builder: CommandBuilder = {};
 
 export const handler = async (argv: Argv) => {
   const cwd = process.cwd();
-  const config = require(path.join(cwd, 'webpack.config.js'))({ production: true });
+  const config = require(path.join(cwd, 'webpack.config.js'))({
+    production: true,
+  });
   const webpack = require(path.join(cwd, 'node_modules/webpack'));
 
   const serverPort = 8888;
   const reportPath = '/tmp/lighthouse.html';
 
-  const server = new Koa();
-
-
-
+  const app = new Koa();
 
   try {
     console.log('compiling app');
     await webpackCompile(webpack, config);
 
     console.log('serving app');
-    server.use(compress());
-    server.use(
+    app.use(compress());
+    app.use(
       staticfiles(path.join(cwd, 'dist'), {
         maxage: 31536000000,
-      })
+      }),
     );
 
-    await server.listen(serverPort);
+    const server = http2
+      .createSecureServer(
+        {
+          ca: await caCrt(),
+          key: await key(),
+          cert: await cert(),
+        },
+        app.callback(),
+      )
+      .listen(serverPort);
 
     console.log('testing app');
-    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
-    const { report } = await lighthouse(`http://localhost:${serverPort}`, {
+    const chrome = await chromeLauncher.launch({
+      chromeFlags: ['--ignore-certificate-errors', '--headless'],
+    });
+    const { report } = await lighthouse(`https://localhost:${serverPort}`, {
       port: chrome.port,
       output: 'html',
     });
 
     console.log('showing results');
-    await new Promise((resolve) => fs.writeFile(reportPath, report, () => resolve()));
+    await fs.writeFile(reportPath, report);
     await opn(reportPath, { wait: false });
 
+    console.log('shutting down');
+    await server.close();
     await chrome.kill();
-
   } catch (e) {
     console.error(e);
-    process.exit(1)
+    process.exit(1);
   }
-
-  return server;
 };
---
+++ src/utils/ssl.ts
@@ -0,0 +1,44 @@
+import pem from 'pem';
+import path from 'path';
+import { promises } from 'fs';
+const fs = promises;
+
+interface Certificate {
+  serviceKey: string;
+  clientKey: string;
+  csr: string;
+  certificate: string;
+}
+
+let memoizedCert: Certificate;
+
+const genCert = async (): Promise<Certificate> => {
+  if (memoizedCert) return Promise.resolve(memoizedCert);
+
+  return new Promise(async (resolve, reject) => {
+    pem.config({
+      pathOpenSSL: '/usr/local/opt/openssl/bin/openssl',
+    });
+    pem.createCertificate(
+      {
+        days: 1,
+        selfSigned: false,
+        serviceCertificate: await caCrt(),
+        serviceKey: await caKey(),
+      },
+      (err, certificate: Certificate) => {
+        if (err) reject(err);
+        memoizedCert = certificate;
+        resolve(certificate);
+      },
+    );
+  });
+};
+
+export const caKey = async (): Promise<string> =>
+  fs.readFile(path.join(__dirname, '../../data/caKey.pem'), 'utf-8');
+export const caCrt = async (): Promise<string> =>
+  fs.readFile(path.join(__dirname, '../../data/caCrt.pem'), 'utf-8');
+
+export const key = async (): Promise<string> => (await genCert()).clientKey;
+export const cert = async (): Promise<string> => (await genCert()).certificate;
